0dcb1ec1a6bef0880203939f665c3693a5c8f61e
mayankpanse83-tech <mayankpanse83@gmail.com>
2026-08-28 06:47:07 +0530

    Issue solwe

---
+++ src/apps/routes/index.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import {
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import EmployeeRoutes from "../apps/employee/routes";
+import ManagerRoutes from "../apps/manager/routes";
+
+import Login from "../apps/employee/component/pages/Login";
+import AccountActivation from "../apps/employee/component/pages/AccountActivation";
+
+
+export default function AppRouter() {
+  return (
+    <Routes>
+
+      {/* ROOT */}
+      <Route
+        path="/"
+        element={
+          <Navigate
+            to="/employee/dashboard"
+            replace
+          />
+        }
+      />
+
+      {/* LOGIN */}
+      <Route
+        path="/login"
+        element={<Login />}
+      />
+
+      {/* ACCOUNT ACTIVATION */}
+      <Route
+        path="/account-activation"
+        element={<AccountActivation />}
+      />
+
+      {/* EMPLOYEE */}
+      <Route
+        path="/employee/*"
+        element={<EmployeeRoutes />}
+      />
+
+      {/* MANAGER */}
+      <Route
+        path="/manager/*"
+        element={<ManagerRoutes />}
+      />
+
+      {/* UNKNOWN */}
+      <Route
+        path="*"
+        element={
+          <Navigate
+            to="/employee/dashboard"
+            replace
+          />
+        }
+      />
+
+    </Routes>
+  );
+}
