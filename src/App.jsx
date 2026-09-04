0ee0b46acb55cadb10f88894e6d15877c4b10477
mayankpanse83-tech <mayankpanse83@gmail.com>
2026-09-04 19:08:53 +0530

    DailyUpdates Done

--- src/App.jsx
+++ src/App.jsx
@@ -370,7 +370,7 @@ function ManagerLayout() {
 
             <Route
   path="/manager/daily-updates"
-  element={<ManagerDailyUpadates />}
+  element={<ManagerDailyUpdates />}
 />
 
           </Routes>
